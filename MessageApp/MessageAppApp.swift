cc7e804599cb04199471432bb35569db51075d05
Ahmed Abdelaziz <ahmedabdelazz2001@gmail.com>
2023-07-09 19:15:32 +0300

    Initial Commit

---
+++ MessageApp/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  MessageApp
+//
+//  Created by Ahmed Abdelaziz on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
